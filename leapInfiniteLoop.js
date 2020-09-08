feeb0aa75cb9b7dfd08c5c598cdd551c49781e4b
Evan Fisher <34456732+EvanRFisher@users.noreply.github.com>
2020-09-07 20:35:53 -0400

    Deliverable 1, step 48

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,6 +1,6 @@
 var controllerOptions = {};
-var i = 0;
+var x = window.innerWidth/2;
+var y = window.innerHeight/2;
 Leap.loop(controllerOptions, function(frame){
-  console.log(i);
-  i=i+1
+  circle(x,y,50);
 });
