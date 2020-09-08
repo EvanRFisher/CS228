8f8cdc5db778111633e00ca2df895b1fe22eb8e3
Evan Fisher <34456732+EvanRFisher@users.noreply.github.com>
2020-09-07 20:37:26 -0400

    Deliverable 1, step 50

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -2,6 +2,7 @@ var controllerOptions = {};
 var x = window.innerWidth/2;
 var y = window.innerHeight/2;
 Leap.loop(controllerOptions, function(frame){
+  clear();
   circle(x,y,50);
   x+=Math.random()*2-1
 });
