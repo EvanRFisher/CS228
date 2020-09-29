125685a997d7ce450bf949e98a6ee94b5503977c
Evan Fisher <34456732+EvanRFisher@users.noreply.github.com>
2020-09-29 00:02:16 -0400

    D4 Step 9

--- Predict.js
+++ Predict.js
@@ -1,3 +1,16 @@
+trainingCompleted = false;
+
 function draw(){
   clear();
+  if (!trainingCompleted) {
+    Train();
+  }
+  Test();
+}
+function Train(){
+  console.log("I am being trained.");
+  trainingCompleted = true;
+}
+function Test(){
+  console.log("I am being tested.");
 }
